8958cc22b858695088448429dd5444ddd2eb890e
Marc <marc.jahservant@gmail.com>
2018-08-04 22:46:44 -0400

    Initial Commit

---
+++ js/splitobtain945.js
@@ -0,0 +1,7 @@
+function splitButtonPressed() {
+    var inputString = document.getElementById("inputVal").value;
+    var tokens = inputString.split(" ");
+    document.getElementById("output").value = tokens.join("\n");
+    document.getElementById("outputSubstring").value = inputString.substring(0, 10);
+    }
+    
